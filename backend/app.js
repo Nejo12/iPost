0ad370abd165b5fb130e95da83c5ecdf89fd41fc
Gabriel <olaniyi_g@hotmail.com>
2019-06-21 15:36:40 +0200

    CORS... setHeader... Applied

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,35 @@
 const express = require("express");
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,  POST, PATCH,DELETE, OPTIONS"
+  );
+  next();
+});
+
 app.use("/api/posts", (req, res, next) => {
   const posts = [
     {
       id: "sdnfeif",
       title: "First server-side post",
-      content: "This is coming from the server"
+      content: "This is coming from the server [1]"
     },
     {
       id: "elrf4",
       title: "Second server-side post",
-      content: "This is coming from the server"
+      content: "This is coming from the server [2]"
     },
     {
       id: "4rfg4r",
       title: "Third server-side post",
-      content: "This is coming from the server"
+      content: "This is coming from the server [3]"
     }
   ];
   res.status(200).json({
