235157d8b6f1b38005071a1523d755f1bbf48a41
Gabriel <olaniyi_g@hotmail.com>
2019-06-24 11:10:59 +0200

    Singup Login implemented on Backend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,32 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 
 const router = express.Router();
 
-router.post("/signup", (req, res, next) => {});
+const User = require("../models/user");
+
+router.post("/signup", (req, res, next) => {
+  bcrypt.hash(req.body.password, 10).then(hash => {
+    const user = new User({
+      email: req.body.email,
+      password: hash
+    });
+    user
+      .save()
+      .then(result => {
+        console.log(result);
+        res.status(201).json({
+          message: "User created.",
+          result: result
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: "Error in Saving User",
+          error: err
+        });
+      });
+  });
+});
 
 module.exports = router;
