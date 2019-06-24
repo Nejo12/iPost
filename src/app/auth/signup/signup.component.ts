d1ec3294bb4cd29779c20b7f263e7bb397204a56
Gabriel <olaniyi_g@hotmail.com>
2019-06-24 19:55:32 +0200

    Connecting front to backend. New user creation possible

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { AuthService } from "../auth.service";
 
 @Component({
   selector: "app-signup",
@@ -9,11 +10,12 @@ import { NgForm } from "@angular/forms";
 export class SignupComponent implements OnInit {
   isLoading: false;
 
-  onSignup(form: NgForm) {
-    console.log(form.value);
-  }
+  constructor(public authService: AuthService) {}
 
-  constructor() {}
+  onSignup(form: NgForm) {
+    if (form.invalid) return;
+    this.authService.createUser(form.value.email, form.value.password);
+  }
 
   ngOnInit() {}
 }
