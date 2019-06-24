78e7e5468a33861cb6dbbac7eacf73a0cbf4193f
Gabriel <olaniyi_g@hotmail.com>
2019-06-24 20:30:40 +0200

    app Authentication implemented

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
@@ -27,6 +28,36 @@ router.post("/signup", (req, res, next) => {
         });
       });
   });
+
+  router.post / "/login",
+    (req, res, next) => {
+      User.findOne({ email: req.body.email })
+        .then(user => {
+          if (!user) {
+            return res.status(401).json({
+              message: "User does not exist."
+            });
+          }
+          return bcrypt.compare(req.body.password, user.password);
+        })
+        .then(result => {
+          if (!result) {
+            return res.status(401).json({
+              message: "Auth Failed!"
+            });
+          }
+          const token = jwt.sign(
+            { email: user.email, userId: user._id },
+            "secret_this_should_be_longer",
+            { expiresIn: "1h" }
+          );
+        })
+        .catch(err => {
+          return res.status(401).json({
+            message: "Auth failed!"
+          });
+        });
+    };
 });
 
 module.exports = router;
